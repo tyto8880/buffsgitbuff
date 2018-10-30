8298011d9584e2f1c6bb206219c1458c6a053bdb
Daniel Taylor (Ambulato) <Isomorph@protonmail.com>
2018-10-30 15:13:55 -0600

    Added more general/cardio pseudoscript

--- integrationScripts/workoutGen.py
+++ integrationScripts/workoutGen.py
@@ -6,42 +6,6 @@ import pymongo
 client = pymongo.MongoClient("localhost",27017)
 db = client.test
 
-user = db.users.find()[0]
-time = None
-muscleGroups = ["arms"]
-
-# print(db.testCollection.find()[0]["x"])#get the zeroth item from the database and print its value for the key "x"
-
-"""
-expect the exercise coll to look like:
-
-primary key: "exercise name" or exerciseid
-"baseReps": <float>//float to make sure we can deal with continuous exercises such as "run for 10 minutes"
-"baseSets": <int>
-"difficultyCardioMult": <float>
-"difficultyStrengthMult": <float>
-"muscleGroups": <list of str>
-"timePerRep": <float>
-
-expect the workout coll to look like:
-
-primary key: workoutid
-"sets": <list of (<list of (exercises [foreign key], reps [float])>,sets [int])>
-"difficultyCardio": <float elt (0,10]>
-"difficultyStrength": <float elt (0,10]>
-
-expect the user coll to look like:
-
-primary key: userid
-"favoriteExercises": <list of (exercise [foreign key],timeSinceLastDone [int, days], lastReps)>
-OR
-"favoriteWorkouts": <list of (workout [foreign key],timeSinceLastDone [int, days], rating [float or int elt [0,100]]>
-"passhsh": <password hash str>
-"cardio": <float elt (0,10]>
-"strength": <float elt (0,10]>
-
-"""
-
 REST_TIME_BASE_SET = 5*60#seconds
 REST_TIME_BASE_REP = 40#seconds
 EXERCISE_DROP_PROBABILITY = 0.05
@@ -50,11 +14,12 @@ EXERCISE_DROP_PROBABILITY = 0.05
 user is the actual item from the database
 time is a range of acceptable times, stored as a tuple of (min,max); IGNORED FOR NOW
 muscles is a list of desired muscle ids to get
-
-this version won't look for existing workouts yet
 """
-def createWorkoutWeights(user, time, muscles):
-	viableExercises = db.exercises.find({"exerciseClass":"strength"})
+def createWorkout(user, time, muscles, isWeights):
+	if(isWeights):
+		viableExercises = db.exercises.find({"exerciseClass":"strength"})
+	else:
+		viableExercises = db.exercises.find({"exerciseClass":"cardio"})
 
 	# dropCardioThreshold = user["cardio"]#anything over this threshold is automatically not included
 	# dropStrengthThreshold = user["strength"]#maybe make these more complex
@@ -85,12 +50,41 @@ def createWorkoutWeights(user, time, muscles):
 		while(random.uniform(0,1) < EXERCISE_DROP_PROBABILITY):
 			heappop(includedExercises)
 		exc = heappop(includedExercises)[1]
-		numSets = exc["baseSets"] * (user["cardio"] / 10) * (10 / (user["strength"]))
-		numReps = exc["baseReps"] * (user["cardio"] / 10) * (10 / (user["strength"]))
-		ttimeTemp = totalTime exc["timePerBaseSet"] * (exc["baseSets"] / numSets) + (REST_TIME_BASE_REP * (10 / user["cardio"]))
-		ttimeTemp += REST_TIME_BASE / user["cardio"]
+		if(isWeights):
+			numSets = exc["baseSets"] * (user["cardio"] / 10) * (10 / (user["strength"]))
+			numReps = exc["baseReps"] * (user["cardio"] / 10) * (10 / (user["strength"]))
+			ttimeTemp = totalTime exc["timePerBaseSet"] * (exc["baseSets"] / numSets) + (REST_TIME_BASE_REP * (10 / user["cardio"]))
+			ttimeTemp += REST_TIME_BASE / user["cardio"]
+		else:
+			if exc["unitType"] == "time":
+				time = exc["baseTime"] * (user["cardio"] / 10)
+			else:
+				#dist
+				time = (exc["baseSpeed"] / exc["baseDist"]) * (user["cardio"] / 10)#endurance here?
+			ttimeTemp = time + REST_TIME_BASE / user["cardio"]
 		if not((ttimeTemp > time[1]) and not (len(includedExercises) == 0)):
-			workout.append(exc)
+			workout.append([exc])
 			totalTime = ttimeTemp
-		
 	return workout
+
+"""
+workout var is list of [exercise,sets,reps] or [exercise,time(s) or dist(m)] like generated above
+want to use user's preferred units, base units are si
+"""
+def workoutToStringList(workout,user):
+	wkt = []
+	isWeights = workout[0][0]["exerciseClass"] == "strength"
+	isDist = False
+	if not isWeights:
+		isDist = workout[0][0]["unitType"] == "distance"
+	for exercise in workout:
+		excname = exercise[0]["exercise"]
+		if isWeights:
+			excq = exercise[1] + "x" + exercise[2]
+		else:
+			if isDist:
+				excq = exercise[1] + " m" if exercise[1] < 1000 else exercise[1] + " km"
+			else:
+				excq = exercise[1] + " sec" if exercise[1] < 60 else ((exercise[1] / 60) + " min" if ((exercise[1]/60) < 60) else (exercise[1]/3600) + " hr")
+		wkt.append(excname + ", " + excq)
+	return wkt
