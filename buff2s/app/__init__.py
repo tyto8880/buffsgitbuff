2e9c91534cef8558f13aac708da1a53a16c81e95
Tyler Tokumoto <tyto8880@colorado.edu>
2018-11-06 11:52:15 -0800

    Flask utilizing static files -- relative symlink to 'Front End HTML' directory to avoid confusion

--- buff2s/app/__init__.py
+++ buff2s/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config import Config
 
 # creates new app instance
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 # loads configuration vars
 app.config.from_object(Config)
 
--- buff2s/app/routes.py
+++ buff2s/app/routes.py
@@ -1,8 +1,10 @@
 # imports application module from app directory
 from app import app
-#import template class
+
+# import template class
 from flask import render_template
 from app.forms import LoginForm
+
 
 # base webpage
 @app.route('/')
@@ -10,7 +12,14 @@ from app.forms import LoginForm
 def home():
     return render_template('home.html', title='Home')
 
+
 @app.route('/login')
 def login():
     form = LoginForm()
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/signup')
+def test():
+    # return render_template('home.html', title='Home')
+    return app.send_static_file('signup.html')
