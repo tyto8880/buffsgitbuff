39c924624ede292c209220684462f6da78c95895
Aaron <aalu3682@colorado.edu>
2018-11-27 16:38:33 -0700

    Catch up with latest commit

---
+++ buff2s/app/ref_routes.py
@@ -0,0 +1,57 @@
+# imports application module from app directory
+from app import app
+from flask import render_template, request, redirect, session
+# local imports for some of that sweet sweet sugar
+import app.database as db
+import app.validation as validate
+
+@app.route('/user/<user>')
+def userdash(user):
+    return render_template('user.html')
+# base webpage
+@app.route('/')
+def root():
+    return render_template('home.html', title='Home')
+
+# cleans up home/root disparity
+@app.route('/home')
+def home():
+    return redirect('/')
+
+# login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        # handle user not present
+        if validate.valid_user(request.form['username'], request.form['password']) and db.get_user_info(request.form['username'], request.form['password']):
+            session.clear()
+            session['username'] = request.form['username']
+            session['logged_in'] = True
+            return redirect('/user/' + request.form['username'])
+        else:
+            error = 'Username/password is incorrect!'
+    return render_template('login.html', error=error)
+
+# signup page
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    error = None
+    if request.method == 'POST':
+        # provide handling for taken user and invalid username
+        if validate.valid_user(request.form['username'], request.form['password']):
+            if not db.get_user_info(request.form['username'], request.form['password']):
+                db.create_user(request.form['username'], request.form['password'])
+            else:
+                error = 'Username already taken!'
+        else:
+            error = 'Invalid username/password!'
+        if not error:
+            session['username'] = request.form['username']
+    return render_template('signup.html', error=error)
+
+@app.route('/logout')
+def logout():
+    if session['username']:
+        session.clear()
+    return redirect('/home')
--- buff2s/app/routes.py
+++ buff2s/app/routes.py
@@ -2,7 +2,7 @@
 from app import app
 
 # import template class
-from flask import render_template
+from flask import render_template, session, request, url_for, flash, redirect
 from app.forms import LoginForm
 
 
@@ -13,13 +13,33 @@ def home():
     return render_template('home.html', title='Home')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
+    try:
+        if request.method == "POST":
+            if request.form['username'] == "Aaron":
+                session['logged_in'] = True
+                session['username'] = request.form['username']
+                return redirect(url_for('home'))
+            else:
+                flash("No!")
+
+    except Exception as e:
+        flash(e)
+        return render_template('login.html', title='Sign In')
+
+    return render_template('login.html', title='Sign In')
 
 
 @app.route('/signup')
 def test():
     # return render_template('home.html', title='Home')
     return app.send_static_file('signup.html')
+
+
+@app.route('/logout')
+def logout():
+    session['logged_in'] = False
+    session.pop('username', None)
+    flash("Logged Out")
+    return render_template('home.html')
