7b948e9c0fe7f8a71feeb4c0721c207606893e35
Daniel Taylor (Ambulato) <Isomorph@protonmail.com>
2018-11-27 15:51:14 -0700

    database.py naming conventions standardized

--- buff2s/app/database.py
+++ buff2s/app/database.py
@@ -1,10 +1,12 @@
 import pymongo
 from passlib.hash import pbkdf2_sha256 as pl
-import heapq
+import heapq as hq
+import numpy as np
 
 client = pymongo.MongoClient("localhost", 27017)
-db = client.test
+db = client.buffs
 
+BASE_TIME_PER_REP = 3#seconds
 REST_TIME_BASE_SET = 5*60  # seconds
 REST_TIME_BASE_REP = 40  # seconds
 EXERCISE_DROP_PROBABILITY = 0.05
@@ -17,8 +19,8 @@ pwd is the user's password (not sure if we'd pass this or just the password hash
 def get_user_info(uname, pwd):
 	info = db.users.find_one({"username": uname})
 
-	if not info or (info and not pl.verify(pwd, info["pwh"])):
-		return None
+	if not info or info and not pl.verify(pwd, info["passwordHash"]):
+		return False
 
 	return info
 
@@ -28,10 +30,10 @@ def create_user(uname, pwd):
 	if db.users.find_one({"username": uname}) is not None:
 		# a user with this username already exists
 		return False
-	
-	pwh = pl.hash(pwd)  # salt included in the hash
+
+	passwordHash = pl.hash(pwd)  # salt included in the hash
 	db.users.insert_one({"username": uname,
-						"pwh": pwh,
+						"passwordHash": passwordHash,
 						"favoriteExercises": [],
 						"cardio": -1,
 						"strength": -1})
@@ -41,11 +43,11 @@ def create_user(uname, pwd):
 # delete this user. Don't do anything if it doesn't exist or they had the wrong password
 def del_user(uname, pwd):
 	q = db.Users.find_one({"username":uname})
-	if (q is None) or not pl.verify(pwd,q["pwh"]):
+	if (q is None) or not pl.verify(pwd,q["passwordHash"]):
 		return False
 	db.Users.deleteOne({"_id":q["_id"]})
 
-'''
+
 """
 user is the actual item from the database
 time is a range of acceptable times, stored as a tuple of (min,max); IGNORED FOR NOW
@@ -57,9 +59,6 @@ def createWorkout(user, time, muscles, isWeights):
 	else:
 		viableExercises = db.exercises.find({"exerciseClass":"cardio"})
 
-	# dropCardioThreshold = user["cardio"]#anything over this threshold is automatically not included
-	# dropStrengthThreshold = user["strength"]#maybe make these more complex
-
 	includedExercises = []#maxheap of exercises
 
 	for exercise in viableExercises:
@@ -70,34 +69,29 @@ def createWorkout(user, time, muscles, isWeights):
 				include = True
 		if include:
 			exercisePriority = 0.0
-			if exercise["exerciseid"] in user["favoriteExercises"]:
-				#
+			if exercise["exerciseID"] in user["favoriteExercises"]:
 				userFavorite = user["favoriteExercises"].index(exercise["exerciseid"])
 				exercisePriority = user["favoriteExercises"][userFavorite][1] + 1.0/userFavorite#assuming favoriteExercises is sorted in order of "favorite"ness
-				reps = user["favoriteExercises"][userFavorite][2]
+				# reps = user["favoriteExercises"][userFavorite][2]
 			else:
 				exercisePriority = 100
-			heappush(includedExercises,(exercisePriority,exercise))
+			hq.heappush(includedExercises,(exercisePriority,exercise))
 
 	workout = []
 	totalTime = 0
 
 	while(totalTime < time[0]):
-		while(random.uniform(0,1) < EXERCISE_DROP_PROBABILITY):
-			heappop(includedExercises)
-		exc = heappop(includedExercises)[1]
+		while(np.random.uniform(0,1) < EXERCISE_DROP_PROBABILITY):
+			hq.heappop(includedExercises)
+		exc = hq.heappop(includedExercises)[1]
 		if(isWeights):
-			numSets = exc["baseSets"] * (user["cardio"] / 10) * (10 / (user["strength"]))
-			numReps = exc["baseReps"] * (user["cardio"] / 10) * (10 / (user["strength"]))
-			ttimeTemp = totalTime exc["timePerBaseSet"] * (exc["baseSets"] / numSets) + (REST_TIME_BASE_REP * (10 / user["cardio"]))
-			ttimeTemp += REST_TIME_BASE / user["cardio"]
+			numSets = exc["baseSets"] * (user["cardioLevel"] / 10) * (10 / (user["strengthLevel"]))
+			numReps = exc["baseReps"] * (user["cardioLevel"] / 10) * (10 / (user["strengthLevel"]))
+			ttimeTemp = totalTime + numReps*BASE_TIME_PER_REP/user["cardioLevel"] + (exc["baseSets"] / numSets) + (REST_TIME_BASE_REP * (10 / user["cardioLevel"]))
+			ttimeTemp += REST_TIME_BASE_SET / user["cardioLevel"]
 		else:
-			if exc["unitType"] == "time":
-				time = exc["baseTime"] * (user["cardio"] / 10)
-			else:
-				#dist
-				time = (exc["baseSpeed"] / exc["baseDist"]) * (user["cardio"] / 10)#endurance here?
-			ttimeTemp = time + REST_TIME_BASE / user["cardio"]
+			time = exc["baseTime"] * (user["cardioLevel"] / 10)
+			ttimeTemp = time + REST_TIME_BASE_SET / user["cardioLevel"]
 		if not((ttimeTemp > time[1]) and not (len(includedExercises) == 0)):
 			workout.append([exc])
 			totalTime = ttimeTemp
@@ -111,17 +105,11 @@ def workoutToStringList(workout,user):
 	wkt = []
 	isWeights = workout[0][0]["exerciseClass"] == "strength"
 	isDist = False
-	if not isWeights:
-		isDist = workout[0][0]["unitType"] == "distance"
 	for exercise in workout:
-		excname = exercise[0]["exercise"]
+		excname = exercise[0]["exerciseName"]
 		if isWeights:
 			excq = exercise[1] + "x" + exercise[2]
 		else:
-			if isDist:
-				excq = exercise[1] + " m" if exercise[1] < 1000 else exercise[1] + " km"
-			else:
-				excq = exercise[1] + " sec" if exercise[1] < 60 else ((exercise[1] / 60) + " min" if ((exercise[1]/60) < 60) else (exercise[1]/3600) + " hr")
+			excq = exercise[1] + " sec" if exercise[1] < 60 else ((exercise[1] / 60) + " min" if ((exercise[1]/60) < 60) else (exercise[1]/3600) + " hr")
 		wkt.append(excname + ", " + excq)
 	return wkt
-'''
