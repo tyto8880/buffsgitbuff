e7cb68efac39fc14a7e374172f3774b1bd19bb4a
Daniel Taylor (Ambulato) <Isomorph@protonmail.com>
2018-11-28 22:36:25 -0700

    Added working and tested workoutgen functions as well as updaters for cardio and strength levels; and a testing function for workoutgen

--- buff2s/app/database.py
+++ buff2s/app/database.py
@@ -51,8 +51,9 @@ def createUser(uname, email, pwd):
 						 "email":email,
 						"passwordHash": passwordHash,
 						"favoriteWorkouts": [],
-						"cardioLevel": -1,
-						"strengthLevel": -1})
+						"cardioLevel": 1,
+						"strengthLevel": 1,
+						 "super":False})
 	return True
 
 
@@ -62,6 +63,24 @@ def deleteUser(uname):
 	if q is None:
 		return False
 	db.users.delete_one({"_id":q["_id"]})
+	return True
+
+def updateUserCardioLevel(uname,level):
+	user = db.users.find_one({"username":uname})
+	if user is None:
+		return False
+	if (level < 1) or ((level > 10) and not ("super" in user and user["super"])):
+		return False
+	db.users.update_one({"_id":user["_id"]},{"$set":{"cardioLevel":level}})
+	return True
+
+def updateUserStrengthLevel(uname,level):
+	user = db.users.find_one({"username":uname})
+	if user is None:
+		return False
+	if (level < 1) or ((level > 10) and not ("super" in user and user["super"])):
+		return False
+	db.users.update_one({"_id":user["_id"]},{"$set":{"strengthLevel":level}})
 	return True
 
 '''
@@ -114,7 +133,7 @@ def getWorkoutFromIDForUser(workoutID,username):
 	user = db.users.find_one({"username": username})
 	workout = db.workouts.find_one({"_id": workoutID})
 	if not (user and workout):
-		return False #either the user or the workout didn't exist
+		return None #either the user or the workout didn't exist
 
 	wkclass = workout["exerciseClass"]
 	wkt = {}
@@ -137,7 +156,7 @@ def getWorkoutFromIDForUser(workoutID,username):
 		if not exercise:
 			raise ExerciseNotDefinedException("Exercise with id " + exerciseID + " was not found during building of workout.")
 
-		wkt["exercises"].append(exercise)
+		wkt["exercises"].append(exercise["exerciseName"])
 
 		#want to have mu st mu for someone whose C and S levels is the same as the recommendation
 		#if C == S, mu = recommended
@@ -171,7 +190,7 @@ def getWorkoutFromIDForUser(workoutID,username):
 			sigTime = float(exercise["baseTime"])/4.
 			time = int(round(stats.truncnorm.rvs(a, bTime, loc=muTime, scale=sigTime, size=1)[0], 0))
 			wkt["times"].append(time)
-
+	return wkt
 
 """
 Take a list of desired muscle groups (list of muscleIDs) to hit and make a generalized workout object. Insert that into the database and return its id.
@@ -186,7 +205,8 @@ def createWorkout(muscleIDs, exerciseClass, workoutName):
 		#trim this down to only the ones we care about
 		if exercise["exerciseClass"] == exerciseClass:
 			musclesMatch = False
-			for m in exercise["muscles"]:
+			mlist = [exercise["muscles"]] if type(exercise["muscles"]) == float else exercise["muscles"]
+			for m in mlist:
 				if m in muscleIDs:
 					musclesMatch = True
 					break
@@ -196,10 +216,10 @@ def createWorkout(muscleIDs, exerciseClass, workoutName):
 	#how long should the workout be?
 	#take a random variable for length according to TN(a=1,b=len(viableExercises),mu=6,sig=2)
 	workoutLength = int(round(stats.truncnorm.rvs(1,len(viableExercises),loc=6,scale=2),0))
-	viableExercises = random.shuffle(viableExercises)
-	exercises = viableExercises[:workoutLength]
+	random.shuffle(viableExercises)
+	exercises = [int(viableExercises[i]["_id"]) for i in range(max(len(viableExercises),workoutLength))]
 	existing = db.workouts.find_one({"exercises":exercises})
 	if existing is not None:
 		return existing["_id"]
-	insertInfo = db.workouts.insert_one({"exercises":exercises,"muscles":muscleIDs,"exerciseClass":exerciseClass,"workoutName":workoutName})
-	return insertInfo["inserted_id"]
+	insertInfo = db.workouts.insert_one({"exercises":exercises,"muscles":list(muscleIDs),"exerciseClass":exerciseClass,"workoutName":workoutName})
+	return insertInfo.inserted_id
--- buff2s/databaseTests.py
+++ buff2s/databaseTests.py
@@ -51,6 +51,34 @@ class databaseTester(unittest.TestCase):
 		ret = db.deleteUser("cthulu ftagn")
 		self.assertFalse(ret)
 
+	def testUpdateLevels(self):
+		db.createUser("Bobb","bob@gmail.com","password")
+		#update cardio and strength to 5
+		ret = db.updateUserCardioLevel("Bobb",5)
+		self.assertTrue(ret)
+		ret = db.getUserInfo("Bobb")
+		self.assertEqual(ret["cardioLevel"],5)
+		ret = db.updateUserStrengthLevel("Bobb", 5)
+		self.assertTrue(ret)
+		ret = db.getUserInfo("Bobb")
+		self.assertEqual(ret["strengthLevel"], 5)
+
+		#update cardio and strength to 11
+		ret = db.updateUserCardioLevel("Bobb", 11)
+		self.assertFalse(ret)
+		ret = db.getUserInfo("Bobb")
+		self.assertEqual(ret["cardioLevel"], 5)
+		ret = db.updateUserStrengthLevel("Bobb", 11)
+		self.assertFalse(ret)
+		ret = db.getUserInfo("Bobb")
+		self.assertEqual(ret["strengthLevel"], 5)
+
+		#update cardio and strength for nonexistent user
+		ret = db.updateUserCardioLevel("cthulu ftagn",5)
+		self.assertFalse(ret)
+		ret = db.updateUserStrengthLevel("cthulu ftagn",5)
+		self.assertFalse(ret)
+
 # Main: Run Test Cases
 if __name__ == '__main__':
     unittest.main()
